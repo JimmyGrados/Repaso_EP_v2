9b0e30e5c7a5201a667a70515574b621beaf9c73
Joaquin <joaq11webmovil@gmail.com>
2024-10-12 10:11:51 -0500

    Pregunta 4 Empresas de Seguros

--- app/src/main/java/com/joaquin/myapplication/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/joaquin/myapplication/ui/gallery/GalleryFragment.kt
@@ -4,39 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.joaquin.myapplication.R
 import com.joaquin.myapplication.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
-
-    private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val view: View = inflater.inflate(R.layout.fragment_gallery, container, false)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val seguros = arrayOf(
+            "GlobeGuard Insurance",
+            "Real Safe Journey COverage",
+            "TravelShield Protection",
+            "VoyageSecure",
+            "WanderSure Travel Insurance"
+        )
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
+        val listView: ListView = view.findViewById(R.id.lvSeguros)
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, seguros)
+        listView.adapter = adapter
+
+        return view
     }
 }
